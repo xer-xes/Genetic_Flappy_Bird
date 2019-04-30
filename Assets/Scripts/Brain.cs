f9d6071dc47f7f99d079de4095046a7e86b732f6
xer-xes <31579450+xer-xes@users.noreply.github.com>
2019-04-30 08:10:44 +0530

    DNA and Brain
    Added DNA and Brain Code

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    int DNA_Length = 5;
+    public DNA DNA;
+    public GameObject Eyes;
+    bool See_Down_Wall = false;
+    bool See_Up_Wall = false;
+    bool See_Top = false;
+    bool See_Bottom = false;
+    bool Alive = true;
+    Vector2 Start_Position;
+    public float Time_Alive = 0;
+    public float Distance_Travelled = 0;
+    public int Crash = 0;
+    Rigidbody2D RB;
+
+    void Init()
+    {
+        //Initializes DNA
+        //0 Forward
+        //1 UpWall
+        //2 DownWall
+        //3 NormalUpward
+        DNA = new DNA(DNA_Length, 200);
+        transform.Translate(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f),0);
+        Start_Position = transform.position;
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Top" || collision.gameObject.tag == "Bottom" || 
+            collision.gameObject.tag == "UpWall" || collision.gameObject.tag == "DownWall")
+        {
+            Crash++;
+        }else if(collision.gameObject.tag == "Dead")
+        {
+            Alive = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!Alive) return;
+
+        See_Bottom = false;
+        See_Top = false;
+        See_Down_Wall = false;
+        See_Up_Wall = false;
+
+        RaycastHit2D Hit = Physics2D.Raycast(Eyes.transform.position, Eyes.transform.forward, 1.0f);
+
+        Debug.DrawRay(Eyes.transform.position, Eyes.transform.forward * 1.0f, Color.red);
+        if (Hit.collider != null)
+        {
+            if (Hit.collider.gameObject.tag == "UpWall")
+            {
+                See_Up_Wall = true;
+            }
+            else if (Hit.collider.gameObject.tag == "DownWall")
+            {
+                See_Down_Wall = true;
+            }
+        }
+        Hit = Physics2D.Raycast(Eyes.transform.position, Eyes.transform.up, 1.0f);
+
+        Debug.DrawRay(Eyes.transform.position, Eyes.transform.up * 1.0f, Color.red);
+        if(Hit.collider != null)
+        {
+            if(Hit.collider.gameObject.tag == "Top")
+            {
+                See_Top = true;
+            }
+        }
+        Hit = Physics2D.Raycast(Eyes.transform.position, -Eyes.transform.up, 1.0f);
+
+        Debug.DrawRay(Eyes.transform.position, -Eyes.transform.up * 1.0f, Color.red);
+        if(Hit.collider != null)
+        {
+            if(Hit.collider.gameObject.tag == "Bottom")
+            {
+                See_Bottom = true;
+            }
+        }
+        //Time_Alive = PopulationManager.Elapsed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!Alive) return;
+        //Read DNA
+        float Up_Force = 0.0f;
+        float Forward_Force = 1.0f;
+        if (See_Up_Wall)
+        {
+            Up_Force = DNA.GetGene(0);
+        }
+        else if (See_Down_Wall)
+        {
+            Up_Force = DNA.GetGene(1);
+        }
+        else if (See_Top)
+        {
+            Up_Force = DNA.GetGene(2);
+        }
+        else if (See_Bottom)
+        {
+            Up_Force = DNA.GetGene(3);
+        }
+        else
+        {
+            Up_Force = DNA.GetGene(4);
+        }
+        RB.AddForce(transform.right * Forward_Force);
+        RB.AddForce(transform.up * Up_Force * 0.1f);
+        Distance_Travelled = Vector2.Distance(Start_Position, transform.position);
+    }
+}
---
+++ Assets/Scripts/DNA.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA
+{
+    List<int> Genes = new List<int>();
+    int DNA_Length = 0;
+    int Max_Values = 0;
+
+    public DNA(int l,int v)
+    {
+        DNA_Length = l;
+        Max_Values = v;
+        SetRandom();
+    }
+
+    private void SetRandom()
+    {
+        Genes.Clear();
+        for (int i = 0; i < DNA_Length; i++)
+        {
+            Genes.Add(Random.Range(-Max_Values, Max_Values));
+        }
+    }
+
+    public void SetGene(int pos,int value)
+    {
+        Genes[pos] = value;
+    }
+
+    public int GetGene(int pos)
+    {
+        return Genes[pos];
+    }
+
+    public void Combine(DNA Parent1,DNA Parent2)
+    {
+        for (int i = 0; i < DNA_Length; i++)
+        {
+            Genes[i] = Random.Range(0, 10) < 5 ? Parent1.Genes[i] : Parent2.Genes[i];
+        }
+    }
+
+    public void Mutate()
+    {
+       Genes[Random.Range(0, DNA_Length)] = Random.Range(-Max_Values, Max_Values);
+    }
+}
